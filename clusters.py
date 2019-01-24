9cdd302e3c59523b2306e7496bbf6e07dccf37be
Chris <chrisandrew119@gmail.com>
2019-01-24 12:25:15 +0530

    Center names and clustered center names.

---
+++ clusters.py
@@ -0,0 +1,158 @@
+{
+  "cluster": [
+    "S. V Digital Library Ttd. P. P. V. P Bangalore"
+  ]
+}{
+  "cluster": [
+    "<enter name of scanning centre>"
+  ]
+}{
+  "cluster": [
+    "CC,HYDERABAD",
+    "CCL HYDERABAD",
+    "CCL HYDERBAD",
+    "CCL SCANNING CENTRE ,HYDERABAD",
+    "CCL YDERABAD",
+    "CCL, HYDERABAD",
+    "CCL,HDERABAD",
+    "CCL,HYDERABAD",
+    "CCL,HYDERABAD.",
+    "CCL,HYDERABAD>",
+    "CCL,HYDERBAD",
+    "CCL,Hyderabad",
+    "CCL.HYDERABAD",
+    "CITY CENTRAL LIBRARY",
+    "POORNAPRAJNA VIDYAPEETHA"
+  ]
+}{
+  "cluster": [
+    "University of Hyderabad",
+    "University of Hyderabad ",
+    "University of Pune "
+  ]
+}{
+  "cluster": [
+    "City Central Library, Hyderabad",
+    "City Central Library,Hyderabad",
+    "State Central Library, Hyderabad"
+  ]
+}{
+  "cluster": [
+    "Poornaprajna_Vidyapeetha",
+    "Poornaprajna_vidyapeetha"
+  ]
+}{
+  "cluster": [
+    "city central liberay",
+    "city central library"
+  ]
+}{
+  "cluster": [
+    "DLISVDLMS049SV Digital Library Tirupati ",
+    "S. V. Digital Library Tirupati",
+    "S.V.Digital Library Tirupathi",
+    "S.V.Digital Library Tirupati",
+    "SV Digital  Library Tirupati",
+    "SV Digital Library Tirupathi",
+    "SV Digital Library Tirupati",
+    "SV Digital Library Tirupati ",
+    "SV_Digital_Library_TTD_Tirupati"
+  ]
+}{
+  "cluster": [
+    "Central Library Hyderabad ",
+    "Central Univercity Hyderabad ",
+    "Central University Hyderabad",
+    "Central University Hyderabad ",
+    "ce>Central University Hyderabad "
+  ]
+}{
+  "cluster": [
+    "S.V.Digital Library,TTD  POORNA PRAJNA SAMSHODHANA MANDIRAM  BANGALORE",
+    "S.V.Digital Library,TTD,POORNA PRAJNA VIDHYA PEETH, BANGLORE"
+  ]
+}{
+  "cluster": [
+    "AKCE UNIVERSAL DIGITAL LIBRARY",
+    "AKCE-UNIVERSAL DIGITAL LIBRARY"
+  ]
+}{
+  "cluster": [
+    "Goa University ",
+    "OSmania University ",
+    "Oqmania Uni6ersity ",
+    "Osmania UniversiTy ",
+    "Osmania University",
+    "Osmania University "
+  ]
+}{
+  "cluster": [
+    "IIIT Allahabad ",
+    "IIIT Hyderabad ",
+    "SASTRA , Thanjavur",
+    "cclhyderabad",
+    "iiit, allahabad",
+    "sastrathanjavur"
+  ]
+}{
+  "cluster": [
+    "S.V. DIGITAL LIBRARY, T.T.D.",
+    "S.V.DIGITAL LIBRARY, T.T.D.",
+    "S.V.DIGITAL_LIBRARY_T.T.D.",
+    "S.V_DIGITAL_LIBRARY_T.T.D.",
+    "S_V_DIGITAL_LIBRARY_TTD"
+  ]
+}{
+  "cluster": [
+    " ",
+    "BUL",
+    "CCL",
+    "CCL (hyd)",
+    "CCL HYD",
+    "CCL,HYD",
+    "Empty",
+    "IIITH",
+    "OU",
+    "Others",
+    "PPVP",
+    "RMSC-HYD",
+    "RMSC-IIITH",
+    "S.V.C.l",
+    "S.V.D.L",
+    "S.V.O.R.I",
+    "SCL-HYD",
+    "TTD"
+  ]
+}{
+  "cluster": [
+    "S  V  Digital Library TTD",
+    "S V Digital Libray",
+    "S. V. Digital Library TTD",
+    "S. V. Digital Library Ttd",
+    "S.V.Digital Librarry, T.T.D",
+    "S.V.Digital Library",
+    "S.V.Digital Library,TTD",
+    "S.V.Digital Library_TTD",
+    "S.V.Digital _Library_TTD",
+    "S.V.Digital-Library_TTD",
+    "S.V.Digital_Librarry_T.T.D",
+    "S.V.Digital_Library_T.T.D",
+    "S.V.Digital_Library_T.T.D.",
+    "S.V.Digital_Library_TTD",
+    "S.V.Digtal Library, T.T.D",
+    "S.V.Digtal_Library_T.T.D",
+    "S.V_Digital _Library_TTD",
+    "SV Digital  Library TTD",
+    "SV Digital Library TTD",
+    "SV Digital Library TTD ",
+    "SV_Digital_Library",
+    "S_V_Digital_Library_PPVP",
+    "S_V_Digital_Library_TTD"
+  ]
+}{
+  "cluster": [
+    "S.V DIGITAL LIBRARY,TTD.P.P.V.P BANGALORE",
+    "S.V.DIGITAL LIBRARY,TTD TIRUPATI",
+    "SV DIGITAL LIBRARY,TTD TIRUPATI"
+  ]
+}
